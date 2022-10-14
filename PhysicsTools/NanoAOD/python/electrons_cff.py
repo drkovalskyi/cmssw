6ffd127e313726b985996bbffbe8abe8eb9a83de
Dmytro Kovalskyi <dmytro@cern.ch>
2022-10-14 21:34:32 +0200

    lowered electron pt to 2GeV

--- PhysicsTools/NanoAOD/python/electrons_cff.py
+++ PhysicsTools/NanoAOD/python/electrons_cff.py
@@ -304,7 +304,7 @@ for modifier in run2_miniAOD_80XLegacy, run2_nanoAOD_94X2016:
 
 finalElectrons = cms.EDFilter("PATElectronRefSelector",
     src = cms.InputTag("slimmedElectronsWithUserData"),
-    cut = cms.string("pt > 5 ")
+    cut = cms.string("pt > 2 ")
 )
 
 electronMVATTH= cms.EDProducer("EleBaseMVAValueMapProducer",
